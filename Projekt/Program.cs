15962f0b9a11354ac0beec33b8a898a43f57d044
Johannes Knieling <knieling@dom-itk.local>
2023-09-21 14:30:32 +0200

    Initial commit

---
+++ Projekt/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: knieling
+ * Date: 21.09.2023
+ */
+using System;
+
+namespace Projekt
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// TODO: Softwareprojekt
+			
+			// Warten vor Schließen der Konsole
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
